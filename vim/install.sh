02744593cfbcb7e38317d792a9f04ad7e840978d
Diogo Kersting <diogovk@gmail.com>
2014-03-06 16:30:07 -0300

    Fix typo in vim/install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -5,12 +5,12 @@ mydir="$PWD"
 mkdir -p "$HOME"/github
 cd "$HOME"/github
 git clone https://github.com/amix/vimrc
-ln -s "$PWD/vimrc" "$HOME/.vim_rutime"
-cd "$HOME/.vim_rutime"
+ln -s "$PWD/vimrc" "$HOME/.vim_runtime"
+cd "$HOME/.vim_runtime"
 echo installing awesome_vimrc
 sh install_awesome_vimrc.sh
 cd "$HOME"/.vim_runtime/sources_non_forked
 git clone https://github.com/elixir-lang/vim-elixir
 git clone https://github.com/tpope/vim-rails
-cp -a "$mydir/my_configs.vim" "$HOME/.vim_rutime"
+cp -a "$mydir/my_configs.vim" "$HOME/.vim_runtime"
 
