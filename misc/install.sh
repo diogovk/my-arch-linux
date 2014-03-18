fd0345fdd4d853b1b6021213c23e7703f42e4c99
Diogo V. Kersting <diogovk@gmail.com>
2014-03-17 21:21:05 -0300

    misc

--- misc/install.sh
+++ misc/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+grep -q '^\[multilib\]' /etc/pacman.conf || {
+  echo Enabling multilib in pacman
+  echo '[multilib]' >> /etc/pacman.conf
+  echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+}
+
+
+
 PKGLIST="$(cat pac_list.txt)"
-pacman --needed -Sy --noconfirm $PKGLIST
+pacman --needed -Syu --noconfirm $PKGLIST
